a8018d4fbb646159d8b742406100600f3426333a
Chris Pitkin <cwpitkin@gmail.com>
2014-07-23 17:50:56 -0700

    added image loading function

--- src/main/java/com/cwp/iso_colourator_20x6/app/MainActivity.java
+++ src/main/java/com/cwp/iso_colourator_20x6/app/MainActivity.java
@@ -1,23 +1,30 @@
 package com.cwp.iso_colourator_20x6.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.imageView);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -29,10 +36,31 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_load_image) {
+            Intent choosePhotoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            choosePhotoIntent.setType("image/*");
+            startActivityForResult(choosePhotoIntent, 1);
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK) {
+            if(requestCode == 1) {
+                if(data == null) {
+                    Toast.makeText(this,getString(R.string.general_error),Toast.LENGTH_LONG).show();
+                } else {
+                    Uri imageUri = data.getData();
+                    String imagePath = imageUri.getPath();
+                    img.setImageURI(imageUri);
+                }
+            }
+        } else if(resultCode != RESULT_CANCELED) {
+            Toast.makeText(this, getString(R.string.general_error), Toast.LENGTH_LONG).show();
+        }
     }
 
 }
